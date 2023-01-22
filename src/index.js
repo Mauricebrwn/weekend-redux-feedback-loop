e0071869d03e3da7bfecfcb215f6b1e2d4115544
Maurice Brown <mauricebrwn6890@gmail.com>
2023-01-22 12:12:33 -0600

    indexjs boilerplate set up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, combineReducers,createStore } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedbackList = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_FEEDBACKLIST':
+            return action.payload;
+            default:
+                return state;
+    } 
+}
+
+
+const reduxStore = createStore(
+    combineReducers({
+        feedbackList
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
